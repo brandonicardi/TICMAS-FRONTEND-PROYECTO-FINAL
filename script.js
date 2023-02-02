ba08b83e8e47fdb7c98bbfbb83c476879014f24e
Brandon Icardi <brandonicardi@gmail.com>
2023-02-02 14:16:38 -0300

    Funciones agregadas

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+//Funcion para ocultar o mostrar el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //Ocultamos el menu una vez seleccionada la opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("java");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("dedicacion");
+    }
+}
+
+
+// Detectamos el scrolling para poder animar las barras de habilidad
+window.onscroll = function(){
+    efectoHabilidades();
+} 
